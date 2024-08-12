5b4bbc8bb0cf6b91c09f30fb06b121028bc29d6e
Dineshgit1996 <57143736+Dineshgit1996@users.noreply.github.com>
2024-08-12 14:42:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Dinesh" }');
+    res.send('{ "response": "Hello Nagendra" }');
 });
 
 app.get('/will', function (req, res) {
